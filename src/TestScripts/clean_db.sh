1187e5e73bd2c205bc906d72828f27806e18cc1a
tshyana <yanatsaha0@gmail.com>
2025-10-06 17:34:42 +0300

    fix

--- src/TestScripts/clean_db.sh
+++ src/TestScripts/clean_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-PGPASSWORD="password" psql -h localhost -U postgres -d testdb -p 5433 -f Shared/db/clean-scripts/clean_db.sql > /dev/null 2>&1
+PGPASSWORD="password" psql -h localhost -U postgres -d testdb -p 5433 -f Shared/db/clean-scripts/clean_db.sql
