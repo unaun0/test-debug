8535539e0457d3dfe75233602df09ad6ce559a7c
tshyana <105216500+unaun0@users.noreply.github.com>
2025-10-06 17:31:10 +0300

    Update create_db.sh

--- src/TestScripts/create_db.sh
+++ src/TestScripts/create_db.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 set -e
 
-PGPASSWORD="password"
-HOST="localhost"
-USER="postgres"
-DB="testdb"
-PORT="5433"
-SQL_FILE="Shared/db/init-scripts/create_db.sql"
-
-psql -h "$HOST" -U "$USER" -d "$DB" -p "$PORT" -f "$SQL_FILE" > /dev/null 2>&1
-
+PGPASSWORD="password" psql -h localhost -U postgres -d testdb -p 5433 -f Shared/db/init-scripts/create_db.sql
